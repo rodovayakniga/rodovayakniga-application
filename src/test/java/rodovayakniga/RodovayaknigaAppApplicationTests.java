b8e79f45fc5753045b6752e7169a9d1ccca2e361
Saydum <saydum.it5@gmail.com>
2023-08-28 20:50:55 +0300

    Install Speing

---
+++ src/test/java/rodovayakniga/RodovayaknigaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package rodovayakniga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodovayaknigaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
