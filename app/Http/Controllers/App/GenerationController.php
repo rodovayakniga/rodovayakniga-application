2399cd79058708d4c6e2b2a93003ab2db5e71050
Saydum <saydum.it5@gmail.com>
2023-09-17 20:26:30 +0300

    implement Generation

--- app/Http/Controllers/App/GenerationController.php
+++ app/Http/Controllers/App/GenerationController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\App;
 
 use App\Http\Controllers\CrudBaseController;
+use App\Http\Requests\GenerationRequest;
 use App\Models\Generation;
 
 class GenerationController extends CrudBaseController
 {
     public function __construct()
     {
-        parent::__construct(Generation::class);
+        parent::__construct(
+            Generation::class,
+            GenerationRequest::class,
+        );
     }
 }
