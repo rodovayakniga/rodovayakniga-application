884051122438ecfc5d8e7855d93d1c2cdd443f7d
Saydum <saydum.it5@gmail.com>
2023-09-17 20:27:40 +0300

    mv Api\V1\Auth

--- app/Http/Controllers/Api/V1/Auth/AuthController.php
+++ app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1\Auth;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterUserRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
-use App\Http\Requests\RegisterUserRequest;
 
 class AuthController extends Controller
 {
