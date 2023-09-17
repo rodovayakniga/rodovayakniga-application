183fcf41ccdc59ebaab3dd72d1ce3e55369ebce5
Saydum <saydum.it5@gmail.com>
2023-09-17 20:24:33 +0300

    deleted route in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,5 @@
 <?php
 
-use App\Http\Controllers\App\HumanController;
-use App\Http\Controllers\App\RodovayaknigaController;
-use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 //Route::post('/register',[AuthController::class,'register']);
 //Route::post('/login', [AuthController::class, 'login']);
 //
